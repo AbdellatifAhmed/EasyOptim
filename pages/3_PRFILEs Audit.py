09c88639db488b0e7fdbadc50d30624524113407
AbdellatifAhmed <abdellatif.ahmed@live.com>
2024-12-20 16:19:10 +0300

    Update 3_PRFILEs Audit.py

--- pages/3_PRFILEs Audit.py
+++ pages/3_PRFILEs Audit.py
@@ -11,4 +11,25 @@ st.set_page_config(
 st.title("PRFILE Audit Tool")
 st.write("tool take input PRFILEs and share organized output")
 
-
+st.markdown(
+    """
+    <style>
+    .footer {
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        background-color: #f8f9fa;
+        padding: 10px 0;
+        text-align: left;
+        font-size: 16px;
+        border-top: 2px solid #e0e0e0;
+    }
+    </style>
+    <div class="footer">
+        The Tool developed by Abdellatif Ahmed (abdellatif.ahmed@nokia.com)
+        
+    </div>
+    
+    """,
+    unsafe_allow_html=True,
+)
