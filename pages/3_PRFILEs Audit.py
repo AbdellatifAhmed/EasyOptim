a60414069b48799d54440fbfe12b6aee36b6b422
AbdellatifAhmed <abdellatif.ahmed@live.com>
2024-12-31 22:29:30 +0300

    Update 3_PRFILEs Audit.py

--- pages/3_PRFILEs Audit.py
+++ pages/3_PRFILEs Audit.py
@@ -1,15 +1,18 @@
-import streamlit as st
 import pandas as pd
+import ast
+import streamlit as st
 import tools as lataftaf
-import pydeck as pdk
-from pydeck.types import String
-import base64
 import os
+import base64
 
+output_dir = os.path.join(os.getcwd(), 'OutputFiles')
+xls_PRFILEs = os.path.join(output_dir, 'PRFILE.xlsx')
 st.set_page_config(
-    page_title="EasyOptim - PRFILE Audit",
-    layout="wide"  # Use the full width of the page
+    page_title="EasyOptim - Audit PRFILE",
+    layout="wide"
 )
+
+# Page Header
 st.markdown(
     """
     <style>
@@ -23,38 +26,27 @@ st.markdown(
     }
     </style>
     <div class="header">
-        EasyOptim - PRFILE Check Tool 
+        EasyOptim - Audit PRFILE 
     </div>
     """,
     unsafe_allow_html=True,
 )
+with st.expander("Import RNCs PRFILEs", expanded=True):
+    container1 = st.container()
+    with container1:
+        files_txt = st.file_uploader("Select RNCs PRFILEs:", type=[".txt"], accept_multiple_files=True)
+        btn_checkPRFILE = st.button("Check PRFILEs", key="prfiles_button")
+        if btn_checkPRFILE:
+            output_status = lataftaf.audit_prfiles(files_txt)
+            if output_status == "PRFILEs Preparation Done Successfully!":
+                st.write(output_status)
+                with open(xls_PRFILEs, "rb") as f:
+                    file_data = f.read()
+                    b64_file_data = base64.b64encode(file_data).decode()
+                    download_link = f'<a href="data:application/octet-stream;base64,{b64_file_data}" download="{os.path.basename(xls_PRFILEs)}">Click to download {os.path.basename(xls_PRFILEs)}</a>'
+                st.markdown(download_link, unsafe_allow_html=True)
+            else:
+                st.error(output_status)
 
-DB_work_Log = lataftaf.get_log_file()
-with open(DB_work_Log, "rb") as f:
-    file_data = f.read()
-    b64_file_data = base64.b64encode(file_data).decode()
-    download_link = f'<a href="data:application/octet-stream;base64,{b64_file_data}" download="{os.path.basename(DB_work_Log)}">Click to download {os.path.basename(DB_work_Log)}</a>'
-st.markdown(download_link, unsafe_allow_html=True)
 
-st.markdown(
-    """
-    <style>
-    .footer {
-        position: fixed;
-        bottom: 0;
-        width: 100%;
-        background-color: #f8f9fa;
-        padding: 10px 0;
-        text-align: left;
-        font-size: 16px;
-        border-top: 2px solid #e0e0e0;
-    }
-    </style>
-    <div class="footer">
-        The Tool developed by Abdellatif Ahmed (abdellatif.ahmed@nokia.com)
-        
-    </div>
-    
-    """,
-    unsafe_allow_html=True,
-)
+
