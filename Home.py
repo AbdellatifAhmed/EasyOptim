4a25f9bcd7ccdacf890e988a141a2db9d15142c4
AbdellatifAhmed <abdellatif.ahmed@live.com>
2024-12-31 19:35:38 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,12 +1,12 @@
 import streamlit as st
-import pandas as pd
 import tools as lataftaf
-import pydeck as pdk
 import base64
 import os
+import pandas as pd
+from pathlib import Path
 
 st.set_page_config(
-    page_title="EasyOptim",
+    page_title="EasyOptim - Test Page",
     layout="wide"  # Use the full width of the page
 )
 st.markdown(
@@ -16,7 +16,7 @@ st.markdown(
         background-color: #f8f9fa;
         padding: 20px;
         text-align: left;
-        font-size: 24px;
+        font-size: 30px;
         font-weight: bold;
         border-bottom: 2px solid #e0e0e0;
     }
@@ -27,102 +27,84 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
-st.write("Please select any desired tool, also updated DB can be uploaded")
-recent_dB = lataftaf.get_log() 
 
-with st.container():
-    status = st.empty()
-    try:
-        with open(recent_dB[1], "rb") as f:
-            file_data = f.read()
-        b64_file_data = base64.b64encode(file_data).decode()  # Encode log content to base64
-        href = f'<a href="data:file/txt;base64,{b64_file_data}" download="{os.path.basename(recent_dB[1])}">{recent_dB[0]}</a>'
-        sites_dB_comment = f"The Sites DB file [ {href} ] already exists. Use upload if a recent one needs to be used."
-        status.markdown(sites_dB_comment, unsafe_allow_html=True)
-    except Exception as e:
-        st.error(f"Error reading intial File location: {e}")
+st.write("Please select the desired tool on side bar, also updated DB can be uploaded")
+recent_dB = lataftaf.get_log()
+# try:
+#     with open(recent_dB[1], "rb") as f:
+#         file_data = f.read()
+#     b64_file_data = base64.b64encode(file_data).decode()  # Encode log content to base64
+#     href_KML = f'<a href="data:application/octet-stream;base64,{b64_file_data}" download="{os.path.basename(recent_dB[1])}">{recent_dB[0]}</a>'
     
-    dB_file = st.file_uploader("Sites DB File:", type=["csv"])
-    Is_Update_Nbrs = st.checkbox('Check to update Nbrs')
+#     with open(recent_dB[3], "rb") as f:
+#             file_data = f.read()
+#     b64_file_data = base64.b64encode(file_data).decode()  # Encode log content to base64
+#     href_Dmp = f'<a href="data:application/octet-stream;base64,{b64_file_data}" download="{os.path.basename(recent_dB[3])}">{recent_dB[2]}</a>'
+    
+# except Exception as e:
+#         st.error(f"Error reading intial Files location: {e}")
 
-if st.button("Submit"):
-    if dB_file is not None:
+if "expanded" not in st.session_state:
+    st.session_state.expanded = True
+
+with st.expander("Upload/Update Dump & KML File",expanded=st.session_state.expanded):
+    cont1 = st.container()
+    with cont1:
+        col1,col2 = st.columns(2)
         try:
-            dB_file_name = dB_file.name
-            dB_file_date = dB_file_name [:-4][3:]
-            # Load and clean the data
-            dB_data = pd.read_csv(dB_file, engine='python', encoding='Windows-1252')
-            dB_data = lataftaf.clean_Sites_db(dB_data,Is_Update_Nbrs,dB_file_name)
-            # href = f'<a href="data:file/txt;base64,{b64_file_data}" download="{os.path.basename(recent_dB[1])}">{dB_file_name}</a>'
-            # sites_dB_comment = f"The Sites DB file [ {href} ] already exists. Use upload if a recent one needs to be used."
-            # status.markdown(sites_dB_comment, unsafe_allow_html=True)
-            # Define the polygon layer
-            Sectors_layer = pdk.Layer(
-                "PolygonLayer",
-                dB_data,
-                get_polygon="polygon",  # Use the 'polygon' column
-                get_fill_color="[200, 30, 90, 160]",  # Purple fill color with transparency
-                get_line_color=[255, 255, 255],  # White border
-                pickable=True,  # Enable click interactions
-                extruded=False,  # Flat shapes
-                
-            )
-            
-            # Define the text layer for NodeB IDs
-            text_layer = pdk.Layer(
-                "TextLayer",
-                dB_data,
-                get_position="coordinates",  # Position for the text
-                get_text="NodeB Id",  # Column to display as text
-                get_color=[0, 0, 0, 0],  # Black text with slight transparency
-                get_size=10,  # Font size
-                get_angle=0,
-                get_alignment_baseline="'top'",  # Position text above the sector
-                pickable=False,  # Text should not be clickable
-                
-            )
-            
-            # Define the map view state
-            view_state = pdk.ViewState(
-                latitude=31.95938,  # Center based on average latitude
-                longitude=35.862283,  # Center based on average longitude
-                zoom=15,
-                pitch=0,
-            )
-            
-            # Render the map
-            st.pydeck_chart(
-                pdk.Deck(
-                    layers=[Sectors_layer,text_layer], 
-                    initial_view_state=view_state,
-                    tooltip={"Vendor": "{Vendor}\n{BSCName}"},
-                    map_style="mapbox://styles/mapbox/streets-v11"
-                )
-            )
-        except Exception as e:
-            st.error(f"Error reading Engineer Parameters File: {e}")
-    else:
-        st.error("No Sites DB file slected")
+            col1.markdown("Current KML File: " + recent_dB[0], unsafe_allow_html=True)
+        except:
+            pass
+        file_Kml = col1.file_uploader("Sites DB File:", type=["csv"])
+        col1_1,col1_2 = col1.columns(2)
+        btn_Kml = col1_1.button("Update KML")
+        Is_Update_Nbrs = col1_2.checkbox("Check to update estimated Nbrs")
+        try:
+            col2.markdown("Current Dump File: " + recent_dB[2], unsafe_allow_html=True)
+        except:
+            pass
+        file_Dmp = col2.file_uploader("Param Dump File:", type=["xlsb"])  
+        btn_Dmp = col2.button("Update Dump")
 
-st.markdown(
-    """
-    <style>
-    .footer {
-        position: fixed;
-        bottom: 0;
-        width: 100%;
-        background-color: #f8f9fa;
-        padding: 10px 0;
-        text-align: left;
-        font-size: 16px;
-        border-top: 2px solid #e0e0e0;
-    }
-    </style>
-    <div class="footer">
-        The Tool developed by Abdellatif Ahmed (abdellatif.ahmed@nokia.com)
+
+with st.expander("Upload KPIs & Performance Reports",expanded=st.session_state.expanded):
+    cont2 = st.container()
+    with cont2:
+        col2G,col3G,col4G,col5G = st.columns(4)
+        col2G.file_uploader("2G Performance Report:", type=["csv"])
+        col2G.button("Update 2G Report")
         
-    </div>
-    
-    """,
-    unsafe_allow_html=True,
-)
+        col3G.file_uploader("3G Performance Report:", type=["csv"])
+        col3G.button("Update 3G Report")
+        
+        col4G.file_uploader("4G Performance Report:", type=["csv"])
+        col4G.button("Update 4G Report")
+        
+        col5G.file_uploader("5G Performance Report:", type=["csv"])
+        col5G.button("Update 5G Report")
+
+with st.expander("Cleaned KML File",expanded=st.session_state.expanded):
+    kml_data = pd.read_csv(recent_dB[1], engine='python', encoding='Windows-1252')
+    st.write(kml_data.head())
+
+if btn_Kml:
+    if file_Kml is not None:
+        try:
+            file_Kml_Name = file_Kml.name
+            # Load and clean the data
+            df_Kml = pd.read_csv(file_Kml, engine='python', encoding='Windows-1252')
+            df_Kml = lataftaf.clean_Sites_db(df_Kml,Is_Update_Nbrs,file_Kml_Name)
+            df_Kml["coordinates"] = df_Kml["coordinates"].apply(lambda x: [float(x[0]), float(x[1])] if isinstance(x, (list, tuple)) else None)
+        except Exception as e:
+            st.error(f"Error reading KML CSV File: {e}")
+    else:
+        st.error("No KML CSV file selected")
+if btn_Dmp:
+    if file_Dmp is not None:
+        try:
+            file_Dmp_Name = file_Dmp.name
+            lataftaf.upload_Dmp(file_Dmp)
+        except Exception as e:
+            st.error(f"Error reading Parameters Dump File: {e}")
+    else:
+        st.error("No Parameters Dump file selected")
