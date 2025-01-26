999da44da97f6b14a8093300db02e1153a7edc30
AbdellatifAhmed <abdellatif.ahmed@live.com>
2025-01-26 12:44:04 +0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -188,6 +188,8 @@ def audit_Lnrel(Lnrel_audit_form):
     
     dict_longitude = (dict(zip(fileSitesDB_dF['NodeB'], fileSitesDB_dF['Long'])))
     dict_latitude = (dict(zip(fileSitesDB_dF['NodeB'], fileSitesDB_dF['Lat'])))
+    dict_longitude = dict_longitude[~dict_longitude.index.duplicated(keep='first')]
+    dict_latitude = dict_latitude[~dict_latitude.index.duplicated(keep='first')]
     lnrel_Performance_DF['Source Longitude'] = lnrel_Performance_DF['LNBTS'].map(dict_longitude)
     lnrel_Performance_DF['Source Latitude'] = lnrel_Performance_DF['LNBTS'].map(dict_latitude)
     lnrel_Performance_DF['Target Longitude'] = lnrel_Performance_DF['Target eNB'].map(dict_longitude)
