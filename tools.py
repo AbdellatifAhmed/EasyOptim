bb17b21dfc104f05294ac71a23dbb44e582c481c
AbdellatifAhmed <abdellatif.ahmed@live.com>
2025-01-26 11:30:33 +0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -229,7 +229,7 @@ def get_target_enb_id(x):
 
 def clean_Sites_db(sites_df,Is_Update_Nbrs,dB_file_name):
     sites_df.columns = sites_df.columns.str.strip()
-    sites_df['NodeB Id'] = sites_df['NodeB Name'].apply(lambda x: x.split('-')[0])
+    sites_df['NodeB Id'] = sites_df['NodeB Name'].apply(lambda x: str(x).split('-')[0])
     sites_df['Sector_ID'] = sites_df.apply(lambda row: (str(row['NodeB Id']) +'_' + str(row['Cell Name'])[-2:][:1]), axis=1)
     sites_df["polygon"] = sites_df.apply(lambda row: calculate_sector_polygon(row["Lat"], row["Long"], row["Bore"]), axis=1)
     sites_df["Label"] = sites_df["polygon"].apply(lambda x: x[2])
